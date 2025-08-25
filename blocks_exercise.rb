99737bbe3d18cf2670d229e252888d2d68407f0f
palashkolk <203395566+palashkolk@users.noreply.github.com>
2025-08-25 17:45:48 +0530

    Add few more examples
    Follows along the ODIN project and tries out the
    exercises.

--- blocks_exercise.rb
+++ blocks_exercise.rb
@@ -7,11 +7,24 @@ end
 
 @transactions = [10, -15, 25, 30, -24, -70, 999]
 
-def transaction_statement(&block)
-  @transactions.each(&block)
+# def transaction_statement(&block)
+#   @transactions.each(&block)
+# end
+
+# transaction_statement do |transaction|
+#   p '%0.2f' % transaction # The bank that calls the method can define how it is handled.
+# end
+# OUtput for this section is not same as shown the tutorial
+
+def transaction_statement
+  formatted_transactions = []
+  @transactions.each do |transaction|
+    formatted_transactions << yield(transaction)
+  end
+
+  p formatted_transactions
 end
 
 transaction_statement do |transaction|
-  p '%0.2f' % transaction # The bank that calls the method can define how it is handled.
+  '%0.2f' % transaction
 end
-# OUtput for this section is not same as shown the tutorial
