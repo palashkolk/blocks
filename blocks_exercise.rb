f8c8d5f66af4c561dd7a185d8bc481d9cb0658fd
palashkolk <203395566+palashkolk@users.noreply.github.com>
2025-08-26 16:38:28 +0530

    Add more example code for ruby blocks and procs
    Follows along the ODIN project ruby exercise.

--- blocks_exercise.rb
+++ blocks_exercise.rb
@@ -28,3 +28,58 @@ end
 transaction_statement do |transaction|
   '%0.2f' % transaction
 end
+
+def mad_limbs
+  yield 'cool', 'beans', 'burrito'
+  yield 'hot', 'tomato', 'burrito'
+  puts 'Also, I said,'
+  yield 'very hot', 'banana', 'burrito'
+end
+
+mad_limbs { |adj, noun| puts "I said #{adj} #{noun}!" }
+
+def awesome_method
+  hash = { a: 'apple', b: 'banana', c: 'cookie' }
+  hash.each do |key, value| # rubocop:disable Style/ExplicitBlockArgument
+    yield key, value
+  end
+end
+
+awesome_method { |key, value| puts "#{key}: #{value}" }
+# awesome_method    #gives jumperror when called without argument
+
+def maybe_block
+  puts 'block party' if block_given?
+  puts 'executed anyway'
+end
+
+maybe_block
+maybe_block {}
+
+my_lambda = -> { puts 'my lambda' }
+my_other_lambda = -> { puts 'hello from the other side' }
+my_lambda.call
+my_other_lambda.call
+my_name = ->(name) { puts "hello, #{name} here" }
+my_age = lambda { |age| puts "I am #{age} years old" } # rubocop:disable Style/Lambda
+my_name.call('Sabir')
+my_age.call('39')
+
+# ############Proc########
+a_proc = proc { |name, age| puts "name: #{name}----- age: #{age}" }
+a_proc.call('Sabir', 39)
+a_proc.call(39)
+# Procs does not care about numner of arguments passed whereas
+# lambda will raise error if number of arguments passed does not
+# match the number of arguments declared with.
+a_lambda = -> { return 1 } # rubocop:disable Style/RedundantReturn
+puts a_lambda.call
+
+def my_method
+  a_proc = proc { return }
+  puts 'this line will be printed'
+  a_proc.call # line returns from the embracing method
+  puts 'this line is never reached'
+end
+
+my_method
